3714b6dcee6ad71b627cb70ea6332d1610c93fdc
brianferrao <shirva.brian@gmail.com>
2017-08-12 13:13:13 -0400

    latest code as of saturday Aug 12

--- tasks/dist.copy.js
+++ tasks/dist.copy.js
@@ -20,6 +20,8 @@ module.exports = function(gulp) {
       'public/bower_components/px-typography-design',
       'public/bower_components/px-polymer-font-awesome',
       'public/bower_components/px-data-table',
+      'public/bower_components/jquery',
+      'public/bower_components/highcharts',
 
       'public/elements/dev-guide'
     ];
